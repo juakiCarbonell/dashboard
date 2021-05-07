bf78c72e9508ceeea6023541079aee6ef81beb7e
Joaquín Carbonell Mira <jcarbonell@ingenia.local>
2021-05-07 12:15:40 +0200

    paginas creadas

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const Dashboard = (props: Props) => {
+    return (
+        <div>
+            Hola
+        </div>
+    )
+}
+
+export default Dashboard
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const Login = (props: Props) => {
+    return (
+        <div>
+            Login Page
+        </div>
+    )
+}
+
+export default Login
